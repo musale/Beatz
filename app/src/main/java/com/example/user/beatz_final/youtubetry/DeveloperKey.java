5477c36a8273da60a2bb1595dde3071d7333814d
WinnyTroy <kiraguwinnie@gmail.com>
2018-08-28 11:17:33 +0300

    Remove API key

--- app/src/main/java/com/example/user/beatz_final/youtubetry/DeveloperKey.java
+++ app/src/main/java/com/example/user/beatz_final/youtubetry/DeveloperKey.java
@@ -2,5 +2,5 @@ package com.example.user.beatz_final.youtubetry;
 
 public class DeveloperKey {
 
-    public static final String DEVELOPER_KEY = "AIzaSyBy32LH5Qt6ucVtMMfLDxLgD81aB89N210";
+    public static final String DEVELOPER_KEY = "";
 }
